00ac6d1f8c344ec15c80964871533f9ce19d2295
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-14 05:33:09 -0300

    test: define meeting health summary contract

---
+++ tests/captacao-summary-contract.test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const html = fs.readFileSync('captacao-resumo.html', 'utf8');
+
+const required = [
+  'id="reconciliation"',
+  'id="actionHealth"',
+  'function calcMeetingHealth',
+  'Conferência da equipe',
+  'Acompanhamento das ações'
+];
+
+const missing = required.filter(token => !html.includes(token));
+if (missing.length) {
+  console.error('Captação summary contract missing:', missing.join(', '));
+  process.exit(1);
+}
+
+console.log('Captação summary contract OK');
